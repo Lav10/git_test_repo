762389eb644e7fa1a6fc1cb0e0f8c295759613b4
Lav Chaudhary <lav.chaudhary5@gmail.com>
2020-09-15 17:46:49 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+from datetime import datetime
+
+name = sys.argv[0]
+
+with open('/home/airflow/test.txt','w+') as f:
+    text = f.read()
+    text=text+"\nHello "+str(name)+", the current date and time is "+str(datetime.now())
+    f.write(text)
