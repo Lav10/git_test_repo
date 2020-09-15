a8dd989abd2138adf77cd1738ed107ba7d6f2eaf
Lav Chaudhary <lav.chaudhary5@gmail.com>
2020-09-15 18:15:31 +0530

    third commit

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ name = sys.argv[1]
 
 with open('/home/airflow/test.txt','w+') as f:
     text = f.read()
-    text=text+"\nHello "+str(name)+", the current date and time is "+str(datetime.now())
+    text=text+"\nHello "+str(name)+", the current date and time is "+str(datetime.now())+". And this is check the git code update.\n"
     f.write(text)
